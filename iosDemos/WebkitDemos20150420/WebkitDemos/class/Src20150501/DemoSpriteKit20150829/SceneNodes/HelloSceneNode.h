2d755329899fe9c2e82881570cb36bae0114eb42
iphoneReadingme <iphoneReadingme@users.noreply.github.com>
2015-08-29 21:30:17 +0800

    20150828_ 1. 修改视图来创建场景，并在视图中呈现场景。
    2. 第一个场景显示了传统的“Hello World”文本。
    
    3. 修改场景的拉伸方式

---
+++ iosDemos/WebkitDemos20150420/WebkitDemos/class/Src20150501/DemoSpriteKit20150829/SceneNodes/HelloSceneNode.h
@@ -0,0 +1,28 @@
+
+/*
+ *****************************************************************************
+ * Copyright (C) 2005-2013 readbookamazon@126.com. All Rights Reserved
+ * File			: HelloSceneNode.h
+ *
+ * Description	: HelloSceneNode
+ *
+ * Author		: yangfs@ucweb.com
+ *
+ * Created by yangfs on   2015-08-29.
+ * History		: modify: 2015-08-29.
+ *
+ ******************************************************************************
+ **/
+
+
+#import <SpriteKit/SpriteKit.h>
+
+
+@interface HelloSceneNode : SKScene
+{
+	
+}
+
+- (void)updateScaleMode:(SKSceneScaleMode)scaleMode;
+
+@end
